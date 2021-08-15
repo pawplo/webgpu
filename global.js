551cc386e1f7a6f2ebbdb5d81e7347180d919873
Paweł Płóciennik <none@example.com>
2021-08-15 19:37:24 +0200

    cleanup

--- global.js
+++ global.js
@@ -1,1 +1,6 @@
 export const numParticles = 100;
+
+export function webgpu_error(str)
+{
+  document.getElementById("webgpu_error").innerText = "webgpu error: [ "+str+" ]";
+}
--- index.js
+++ index.js
@@ -2,92 +2,54 @@
 import * as global from "./global.js";
 import * as wgsl from "./wgsl.js";
 
-function webgpu_error(str)
-{
-  document.getElementById("webgpu_error").innerText = "webgpu error: [ "+str+" ]";
-}
-
 async function init() {
-  console.log("init()");
   const canvas = document.querySelector('canvas');
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  if (!navigator.gpu) { webgpu_error("!navigator.gpu"); return; }
+  if (!navigator.gpu) { global.webgpu_error("!navigator.gpu"); return; }
 
   var adapter;
   try { adapter = await navigator.gpu.requestAdapter({}); }
-  catch (e) { webgpu_error(e); return; }
-  if (!adapter) { webgpu_error("navigator.gpu.requestAdapter()"); return; }
+  catch (e) { global.webgpu_error(e); return; }
+  if (!adapter) { global.webgpu_error("navigator.gpu.requestAdapter()"); return; }
 
   var device;
   try { device = await adapter.requestDevice(); }
-  catch (e) { webgpu_error(e); return; }
+  catch (e) { global.webgpu_error(e); return; }
 
   const context = canvas.getContext('webgpu');
-  if (!context) { webgpu_error("canvas.getContext('webgpu')"); return; }
+  if (!context) { global.webgpu_error("canvas.getContext('webgpu')"); return; }
 
   const presentationFormat = context.getPreferredFormat(adapter);
 
-  context.configure({
-    device,
-    format: presentationFormat, //'bgra8unorm',
-  });
+  context.configure({ device, format: presentationFormat, });
 
   const renderPipeline = device.createRenderPipeline({
     vertex: {
-      module:
-            device.createShaderModule({
-            code: wgsl.wgslShaders.vertex,
-          }),
+      module: device.createShaderModule({ code: wgsl.wgslShaders.vertex, }),
       entryPoint: 'main',
       buffers: [
         {
-          // instanced particles buffer
-          arrayStride: 4 * 4,
-          stepMode: 'instance',
+          arrayStride: 4 * 4, stepMode: 'instance',
           attributes: [
-            {
-              // instance position
-              shaderLocation: 0,
-              offset: 0,
-              format: 'float32x2',
-            },
-            {
-              // instance velocity
-              shaderLocation: 1,
-              offset: 2 * 4,
-              format: 'float32x2',
-            },
+            { shaderLocation: 0, offset: 0,     format: 'float32x2', },
+            { shaderLocation: 1, offset: 2 * 4, format: 'float32x2', },
           ],
         },
         {
-          // vertex buffer
-          arrayStride: 2 * 4,
-          stepMode: 'vertex',
+          arrayStride: 2 * 4, stepMode: 'vertex',
           attributes: [
-            {
-              // vertex positions
-              shaderLocation: 2,
-              offset: 0,
-              format: 'float32x2',
-            },
+            { shaderLocation: 2, offset: 0, format: 'float32x2', },
           ],
         },
       ],
     },
     fragment: {
-      module:
-          device.createShaderModule({
-            code: wgsl.wgslShaders.fragment,
-          }),
+      module: device.createShaderModule({ code: wgsl.wgslShaders.fragment, }),
       entryPoint: 'main',
-      targets: [
-        {
-          format: 'bgra8unorm',
-        },
-      ],
+      targets: [{ format: 'bgra8unorm', }, ],
     },
     primitive: {
       topology: 'triangle-list',
@@ -96,10 +58,7 @@ async function init() {
 
   const computePipeline = device.createComputePipeline({
     compute: {
-      module:
-          device.createShaderModule({
-            code: wgsl.wgslShaders.compute,
-          }),
+      module: device.createShaderModule({ code: wgsl.wgslShaders.compute, }),
       entryPoint: 'main',
     },
   });
@@ -107,7 +66,7 @@ async function init() {
   const renderPassDescriptor = {
     colorAttachments: [
       {
-        view: undefined, // Assigned later
+        view: undefined,
         loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
       },
     ],
@@ -128,15 +87,7 @@ async function init() {
   const vertexBufferData = new Float32Array([ -0.01, -0.02, 0.01, -0.02, 0.0, 0.02 ])
   const verticesBuffer = create_buffer(vertexBufferData, GPUBufferUsage.VERTEX);
 
-  const simParamData = new Float32Array([
-    0.04, // deltaT;
-    0.1, // rule1Distance;
-    0.025, // rule2Distance;
-    0.025, // rule3Distance;
-    0.02, // rule1Scale;
-    0.05, // rule2Scale;
-    0.005, // rule3Scale;
-  ]);
+  const simParamData = new Float32Array([0.04, 0.1, 0.025, 0.025, 0.02, 0.05, 0.005, ]);
   const simParamBuffer = create_buffer(simParamData, GPUBufferUsage.UNIFORM);
 
   const initialParticleData = new Float32Array(global.numParticles * 4);
@@ -149,8 +100,7 @@ async function init() {
   const particleBuffers = new Array(2);
   const particleBindGroups = new Array(2);
   for (let i = 0; i < 2; ++i)
-    particleBuffers[i] = create_buffer(initialParticleData,
-              GPUBufferUsage.VERTEX | GPUBufferUsage.STORAGE);
+    particleBuffers[i] = create_buffer(initialParticleData, GPUBufferUsage.VERTEX | GPUBufferUsage.STORAGE);
 
   for (let i = 0; i < 2; ++i) {
     particleBindGroups[i] = device.createBindGroup({
@@ -188,9 +138,7 @@ async function init() {
   let now = Date.now();
 
   function frame() {
-    renderPassDescriptor.colorAttachments[0].view = context
-      .getCurrentTexture()
-      .createView();
+    renderPassDescriptor.colorAttachments[0].view = context.getCurrentTexture().createView();
 
     const commandEncoder = device.createCommandEncoder();
 
